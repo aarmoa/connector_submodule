e3f1ac5a6e5d72cf85d5607d499e3ba86d477246
Nullably <37262506+Nullably@users.noreply.github.com>
2020-08-26 21:19:34 +0800

    (feat) move cryto_com to new location

---
+++ crypto_com_utils.py
@@ -0,0 +1,54 @@
+import math
+import time
+import random
+from typing import Dict, List
+
+
+# deeply merge two dictionaries
+def merge_dicts(source: Dict, destination: Dict) -> Dict:
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge_dicts(value, node)
+        else:
+            destination[key] = value
+
+    return destination
+
+
+# join paths
+def join_paths(*paths: List[str]) -> str:
+    return "/".join(paths)
+
+
+# get timestamp in milliseconds
+def get_ms_timestamp() -> int:
+    return math.floor(time.time() * 1e3)
+
+
+# convert milliseconds timestamp to seconds
+def ms_timestamp_to_s(ms: int) -> int:
+    return math.floor(ms / 1e3)
+
+
+# Request ID class
+class RequestId:
+    """
+    Generate request ids
+    """
+    _request_id: int = 0
+
+    @classmethod
+    def generate_request_id(cls) -> int:
+        cls._request_id += 1
+        # return cls._request_id
+        return math.floor(random.random() * 1e18)
+
+
+def convert_from_exchange_trading_pair(exchange_trading_pair: str) -> str:
+    return exchange_trading_pair.replace("_", "-")
+
+
+def convert_to_exchange_trading_pair(hb_trading_pair: str) -> str:
+    return hb_trading_pair.replace("-", "_")
